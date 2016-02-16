251b4bd3614ab31073091e708c931999dca0c53c
themancalledjakob <jakob@lustlab.net>
2016-02-17 00:57:03 +0100

    added new information

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+$(function () {
+	console.log('This would be the main JS file.');
+	$('.link').click(function(){
+	  var bla = '#lesson_' + $(this).attr('data-index');
+	  $('.lesson').fadeOut(300);
+	  $('#lesson_' + $(this).attr('data-index') ).fadeIn(400);
+	});
+});
